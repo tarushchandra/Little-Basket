40f0c50e1f1367b27c445944e813789800d2fc0e
unknown <tarushchandra6@gmail.com>
2022-11-29 20:52:25 +0530

    api url updated 20

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -54,7 +54,10 @@ router.post("/login", async (req, res) => {
 
     const { password, ...others } = foundUser._doc;
 
-    res.cookie("access_token", accessToken).status(200).json(others);
+    res
+      .cookie("access_token", accessToken, { secure: true, httpOnly: true })
+      .status(200)
+      .json(others);
   } catch (err) {
     res.status(500).json(err);
   }
