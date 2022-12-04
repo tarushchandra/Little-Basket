20d26f3e70824e69709067f6b2be2686b17938ad
unknown <tarushchandra6@gmail.com>
2022-12-04 23:51:41 +0530

    final check 2

--- backend/index.js
+++ backend/index.js
@@ -25,8 +25,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
-    // origin: "https://littlebasket.netlify.app",
+    // origin: "http://localhost:3000",
+    origin: "https://littlebasket.netlify.app",
     credentials: true,
   })
 );
